addd839eae237f20d2656b089e526ca9d0ec10d7
Erfan Loghmani <Elkiscoming@gmail.com>
2021-02-09 23:52:06 +0330

    Use attention in learning

--- jodie.py
+++ jodie.py
@@ -205,14 +205,15 @@ with trange(args.epochs) as progress_bar1:
                             tbatch_full_user_repeat = torch.cat([user_embedding_input, user_embedding_static[tbatch_userids, :]], dim=1).unsqueeze(1).repeat((1, N_CLUSTERS, 1))
                             predicted_weights = model.predict_weight(tbatch_full_user_repeat, tbatch_user_last_word_in_cluster)
 
-                            weight_dynamic = predicted_item_embedding[:, -1]
+                            weight_dynamic = predicted_item_embedding[:, -(item_word_embs.shape[1] + 2)]
+                            weight_word = predicted_item_embedding[:, -(item_word_embs.shape[1] + 1)]
 
                             # CALCULATE PREDICTION LOSS
                             item_embedding_input = item_embeddings[tbatch_itemids, :]
                             # print(weight_dynamic.shape, predicted_item_embedding[:, :args.embedding_dim].shape, item_embedding_input.detach().shape) 
                             loss += torch.sum(torch.exp(weight_dynamic) * MSELoss_no_reduce(predicted_item_embedding[:, :args.embedding_dim], item_embedding_input.detach()).sum(1))
-                            loss += torch.sum(MSELoss_no_reduce(tbatch_user_last_word_in_cluster, item_word_embs_input.unsqueeze(1).repeat((1, N_CLUSTERS, 1))).sum(2) * torch.exp(predicted_weights).squeeze(2) * tbatch_user_saw_cluster)
-                            # loss += torch.sum(MSELoss_no_reduce(predicted_item_embedding[:, args.embedding_dim:-1], item_embedding_static[tbatch_itemids, :]).sum(1))
+                            loss += torch.sum(MSELoss_no_reduce(predicted_item_embedding[:, args.embedding_dim:-(item_word_embs.shape[1] + 2)], item_embedding_static[tbatch_itemids, :]).sum(1))
+                            loss += torch.sum(MSELoss_no_reduce(predicted_item_embedding[:, -item_word_embs.shape[1]:], item_word_embs_input.unsqueeze(1).repeat((1, N_CLUSTERS, 1))).sum(2) * torch.exp(predicted_weights).squeeze(2))
 
                             # UPDATE DYNAMIC EMBEDDINGS AFTER INTERACTION
                             user_embedding_output = model.forward(user_embedding_input, item_embedding_input, timediffs=user_timediffs_tensor, features=feature_tensor_full, select='user_update')
--- library_models.py
+++ library_models.py
@@ -68,9 +68,8 @@ class JODIE(nn.Module):
         print "Initializing linear layers"
         self.linear_layer1 = nn.Linear(self.embedding_dim, 50)
         self.linear_layer2 = nn.Linear(50, 2)
-        self.prediction_layer = nn.Linear(self.user_static_embedding_size + self.item_static_embedding_size + self.num_item_features + self.embedding_dim * 2, self.embedding_dim + 1)
-        self.weight_layer1 = nn.Linear(self.user_static_embedding_size + self.embedding_dim + self.num_item_features, 16)
-        self.weight_layer2 = nn.Linear(16, 1)
+        self.prediction_layer = nn.Linear(self.user_static_embedding_size + self.item_static_embedding_size + self.num_item_features + self.embedding_dim * 2, self.embedding_dim + self.item_static_embedding_size + 2)
+        self.multihead_attn = nn.MultiheadAttention(self.user_static_embedding_size + self.embedding_dim, kdim=self.num_item_features, vdim=self.num_item_features, num_heads=2)
         self.embedding_layer = NormalLinear(1, self.embedding_dim)
         print "*** JODIE initialization complete ***\n\n"
         
@@ -104,8 +103,10 @@ class JODIE(nn.Module):
         X_out = self.weight_layer2(X_out)
         return X_out
 
-    def predict_item_embedding(self, user_embeddings):
+    def predict_item_embedding(self, user_embeddings, user_only_embeddings, item_word_embeddings, user_saw_clusters):
         X_out = self.prediction_layer(user_embeddings)
+        attn_output = self.multihead_attn(query=user_only_embeddings, key=item_word_embeddings, value=item_word_embeddings, mask=user_saw_clusters)
+        X_out = torch.cat([X_out, attn_output], dim=1)
         return X_out
 
 
