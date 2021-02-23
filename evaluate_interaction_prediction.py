e9855e42c21ef0ff16065f10b610ccb86d9ff1b2
Erfan Loghmani <Elkiscoming@gmail.com>
2021-02-23 14:05:00 +0330

    Use tbatching in eval

--- evaluate_interaction_prediction.py
+++ evaluate_interaction_prediction.py
@@ -12,6 +12,7 @@ Paper: Predicting Dynamic Embedding Trajectory in Temporal Interaction Networks.
 
 from library_data import *
 from library_models import *
+import library_models as lib
 import json
 
 # INITIALIZE PARAMETERS
@@ -126,97 +127,134 @@ After this prediction, the errors in the prediction are used to calculate the lo
 This simulates the real-time feedback about the predictions that the model gets when deployed in-the-wild.
 Please note that since each interaction in validation and test is only seen once during the forward pass, there is no data leakage.
 '''
+optimizer.zero_grad()
+reinitialize_tbatches()
+total_loss, loss, total_interaction_count = 0, 0, 0
+
 tbatch_start_time = None
-loss = 0
-# FORWARD PASS
-print("*** Making interaction predictions by forward pass (no t-batching) ***")
+tbatch_to_insert = -1
+tbatch_full = False
+
+# TRAIN TILL THE END OF TRAINING INTERACTION IDX
 with trange(train_end_idx, test_end_idx) as progress_bar:
     for j in progress_bar:
-        progress_bar.set_description('%dth interaction for validation and testing' % j)
+        progress_bar.set_description('Processed %dth interactions' % j)
 
-        # LOAD INTERACTION J
+        # READ INTERACTION J
         userid = user_sequence_id[j]
         itemid = item_sequence_id[j]
+        timestamp = timestamp_sequence[j]
         feature = feature_sequence[j]
         user_timediff = user_timediffs_sequence[j]
         item_timediff = item_timediffs_sequence[j]
+
+        # CREATE T-BATCHES: ADD INTERACTION J TO THE CORRECT T-BATCH
+        tbatch_to_insert = max(lib.tbatchid_user[userid], lib.tbatchid_item[itemid]) + 1
+        lib.tbatchid_user[userid] = tbatch_to_insert
+        lib.tbatchid_item[itemid] = tbatch_to_insert
+
+        lib.current_tbatches_user[tbatch_to_insert].append(userid)
+        lib.current_tbatches_item[tbatch_to_insert].append(itemid)
+        lib.current_tbatches_timestamp[tbatch_to_insert].append(timestamp)
+        lib.current_tbatches_feature[tbatch_to_insert].append(feature)
+        lib.current_tbatches_interactionids[tbatch_to_insert].append(j)
+        lib.current_tbatches_user_timediffs[tbatch_to_insert].append(user_timediff)
+        lib.current_tbatches_item_timediffs[tbatch_to_insert].append(item_timediff)
+        lib.current_tbatches_previous_item[tbatch_to_insert].append(user_previous_itemid_sequence[j])
+        lib.current_tbatches_previous_items[tbatch_to_insert].append(user_previous_itemids_sequence[j])
+        lib.current_tbatches_previous_timestamps[tbatch_to_insert].append(user_previous_timestamp_sequence[j])
+
         timestamp = timestamp_sequence[j]
-        if not tbatch_start_time:
+        if tbatch_start_time is None:
             tbatch_start_time = timestamp
-        itemid_previous = user_previous_itemid_sequence[j]
 
-        # LOAD USER AND ITEM EMBEDDING
-        user_embedding_input = user_embeddings[torch.LongTensor([userid]).to(device)]
-        user_embedding_static_input = user_embeddings_static[torch.LongTensor([userid]).to(device)]
-        item_embedding_input = item_embeddings[torch.LongTensor([itemid]).to(device)]
-        item_embedding_static_input = item_embeddings_static[torch.LongTensor([itemid]).to(device)]
-        feature_tensor = Variable(torch.Tensor(feature).to(device)).unsqueeze(0)
-        user_timediffs_tensor = Variable(torch.Tensor([user_timediff]).to(device)).unsqueeze(0)
-        item_timediffs_tensor = Variable(torch.Tensor([item_timediff]).to(device)).unsqueeze(0)
-        item_embedding_previous = item_embeddings[torch.LongTensor([itemid_previous]).to(device)]
-
-        itemids_history_previous = torch.LongTensor(user_previous_itemids_sequence[j]).to(device)
-        previous_timestamps_tensor = torch.Tensor(user_previous_timestamp_sequence[j]).to(device).unsqueeze(0)
-        timestamps_tensor = torch.Tensor([timestamp]).to(device)
-        item_embeddings_history_previous = item_embeddings[itemids_history_previous, :].unsqueeze(0)
-
-        # print(itemids_history_previous.shape, previous_timestamps_tensor.shape, timestamps_tensor.shape, item_embeddings_history_previous.shape)
-
-        # PROJECT USER EMBEDDING
-        user_projected_embedding = model.forward(user_embedding_input, item_embedding_previous, previous_items_embs=item_embeddings_history_previous, timestamps_tensor=timestamps_tensor, previous_timestamps=previous_timestamps_tensor, timediffs=user_timediffs_tensor, features=feature_tensor, select='project')
-        user_item_embedding = torch.cat([user_projected_embedding, item_embedding_previous, item_embeddings_static[torch.LongTensor([itemid_previous]).to(device)], user_embedding_static_input], dim=1)
-
-        # PREDICT ITEM EMBEDDING
-        predicted_item_embedding = model.predict_item_embedding(user_item_embedding)
-
-        # CALCULATE PREDICTION LOSS
-        loss += MSELoss(predicted_item_embedding, torch.cat([item_embedding_input, item_embedding_static_input], dim=1).detach())
-
-        # CALCULATE DISTANCE OF PREDICTED ITEM EMBEDDING TO ALL ITEMS
-        euclidean_distances = nn.PairwiseDistance()(predicted_item_embedding.repeat(num_items, 1), torch.cat([item_embeddings, item_embeddings_static], dim=1)).squeeze(-1)
-
-        # CALCULATE RANK OF THE TRUE ITEM AMONG ALL ITEMS
-        true_item_distance = euclidean_distances[itemid]
-        euclidean_distances_smaller = (euclidean_distances < true_item_distance).data.cpu().numpy()
-        true_item_rank = np.sum(euclidean_distances_smaller) + 1
-
-        if j < test_start_idx:
-            validation_ranks.append(int(true_item_rank))
-        else:
-            test_ranks.append(int(true_item_rank))
-
-        # UPDATE USER AND ITEM EMBEDDING
-        user_embedding_output = model.forward(user_embedding_input, item_embedding_input, timediffs=user_timediffs_tensor, features=feature_tensor, select='user_update')
-        item_embedding_output = model.forward(user_embedding_input, item_embedding_input, timediffs=item_timediffs_tensor, features=feature_tensor, select='item_update')
-
-        # SAVE EMBEDDINGS
-        item_embeddings[itemid, :] = item_embedding_output.squeeze(0)
-        user_embeddings[userid, :] = user_embedding_output.squeeze(0)
-        user_embeddings_timeseries[j, :] = user_embedding_output.squeeze(0)
-        item_embeddings_timeseries[j, :] = item_embedding_output.squeeze(0)
-
-        # CALCULATE LOSS TO MAINTAIN TEMPORAL SMOOTHNESS
-        loss += MSELoss(item_embedding_output, item_embedding_input.detach())
-        loss += MSELoss(user_embedding_output, user_embedding_input.detach())
-
-        # CALCULATE STATE CHANGE LOSS
-        if args.state_change:
-            loss += calculate_state_prediction_loss(model, [j], user_embeddings_timeseries, y_true, crossEntropyLoss)
-
-        # UPDATE THE MODEL IN REAL-TIME USING ERRORS MADE IN THE PAST PREDICTION
+        # AFTER ALL INTERACTIONS IN THE TIMESPAN ARE CONVERTED TO T-BATCHES, FORWARD PASS TO CREATE EMBEDDING TRAJECTORIES AND CALCULATE PREDICTION LOSS
         if timestamp - tbatch_start_time > tbatch_timespan:
-            tbatch_start_time = timestamp
+            tbatch_start_time = timestamp  # RESET START TIME FOR THE NEXT TBATCHES
+
+            # ITERATE OVER ALL T-BATCHES
+            with trange(len(lib.current_tbatches_user)) as progress_bar3:
+                for i in progress_bar3:
+                    progress_bar3.set_description('Processed %d of %d T-batches ' % (i, len(lib.current_tbatches_user)))
+
+                    total_interaction_count += len(lib.current_tbatches_interactionids[i])
+
+                    # LOAD THE CURRENT TBATCH
+                    tbatch_userids = torch.LongTensor(lib.current_tbatches_user[i]).to(device)  # Recall "lib.current_tbatches_user[i]" has unique elements
+                    tbatch_itemids = torch.LongTensor(lib.current_tbatches_item[i]).to(device)  # Recall "lib.current_tbatches_item[i]" has unique elements
+                    tbatch_interactionids = torch.LongTensor(lib.current_tbatches_interactionids[i]).to(device)
+                    feature_tensor = Variable(torch.Tensor(lib.current_tbatches_feature[i]).to(device))  # Recall "lib.current_tbatches_feature[i]" is list of list, so "feature_tensor" is a 2-d tensor
+                    user_timediffs_tensor = Variable(torch.Tensor(lib.current_tbatches_user_timediffs[i]).to(device)).unsqueeze(1)
+                    item_timediffs_tensor = Variable(torch.Tensor(lib.current_tbatches_item_timediffs[i]).to(device)).unsqueeze(1)
+                    tbatch_itemids_previous = torch.LongTensor(lib.current_tbatches_previous_item[i]).to(device)
+                    item_embedding_previous = item_embeddings[tbatch_itemids_previous, :]
+
+                    tbatch_itemids_history_previous = torch.LongTensor(lib.current_tbatches_previous_items[i]).to(device)
+                    previous_timestamps_tensor = torch.Tensor(lib.current_tbatches_previous_timestamps[i]).to(device)
+                    timestamps_tensor = torch.Tensor(lib.current_tbatches_timestamp[i]).to(device)
+                    item_embedding_previous = item_embeddings[tbatch_itemids_previous, :]
+                    item_embeddings_history_previous = item_embeddings[tbatch_itemids_history_previous, :]
+
+                    # PROJECT USER EMBEDDING TO CURRENT TIME
+                    user_embedding_input = user_embeddings[tbatch_userids, :]
+                    user_projected_embedding = model.forward(user_embedding_input, item_embedding_previous, timestamps_tensor=timestamps_tensor, previous_timestamps=previous_timestamps_tensor, previous_items_embs=item_embeddings_history_previous, timediffs=user_timediffs_tensor, features=feature_tensor, select='project')
+                    user_item_embedding = torch.cat([user_projected_embedding, item_embedding_previous, item_embeddings_static[tbatch_itemids_previous, :], user_embeddings_static[tbatch_userids, :]], dim=1)
+
+                    # PREDICT NEXT ITEM EMBEDDING
+                    predicted_item_embedding = model.predict_item_embedding(user_item_embedding)
+
+                    for en, idx in enumerate(lib.current_tbatches_interactionids[i]):
+                        # CALCULATE DISTANCE OF PREDICTED ITEM EMBEDDING TO ALL ITEMS
+                        euclidean_distances = nn.PairwiseDistance()(predicted_item_embedding[en:en + 1].repeat(num_items, 1), torch.cat([item_embeddings, item_embeddings_static], dim=1)).squeeze(-1)
+
+                        # CALCULATE RANK OF THE TRUE ITEM AMONG ALL ITEMS
+                        true_item_distance = euclidean_distances[tbatch_itemids[en]:tbatch_itemids[en] + 1]
+                        euclidean_distances_smaller = (euclidean_distances < true_item_distance).data.cpu().numpy()
+                        true_item_rank = np.sum(euclidean_distances_smaller) + 1
+
+                        if idx < test_start_idx:
+                            validation_ranks.append(int(true_item_rank))
+                        else:
+                            test_ranks.append(int(true_item_rank))
+
+                    # CALCULATE PREDICTION LOSS
+                    item_embedding_input = item_embeddings[tbatch_itemids, :]
+                    loss += predicted_item_embedding.shape[0] * MSELoss(predicted_item_embedding, torch.cat([item_embedding_input, item_embeddings_static[tbatch_itemids, :]], dim=1).detach())
+
+                    # UPDATE DYNAMIC EMBEDDINGS AFTER INTERACTION
+                    user_embedding_output = model.forward(user_embedding_input, item_embedding_input, timediffs=user_timediffs_tensor, features=feature_tensor, select='user_update')
+                    item_embedding_output = model.forward(user_embedding_input, item_embedding_input, timediffs=item_timediffs_tensor, features=feature_tensor, select='item_update')
+
+                    item_embeddings[tbatch_itemids, :] = item_embedding_output
+                    user_embeddings[tbatch_userids, :] = user_embedding_output
+
+                    user_embeddings_timeseries[tbatch_interactionids, :] = user_embedding_output
+                    item_embeddings_timeseries[tbatch_interactionids, :] = item_embedding_output
+
+                    # CALCULATE LOSS TO MAINTAIN TEMPORAL SMOOTHNESS
+                    loss += MSELoss(item_embedding_output, item_embedding_input.detach())
+                    loss += MSELoss(user_embedding_output, user_embedding_input.detach())
+
+                    # CALCULATE STATE CHANGE LOSS
+                    if args.state_change:
+                        loss += calculate_state_prediction_loss(model, tbatch_interactionids, user_embeddings_timeseries, y_true, crossEntropyLoss)
+
+            # BACKPROPAGATE ERROR AFTER END OF T-BATCH
+            total_loss += loss.item()
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
             # RESET LOSS FOR NEXT T-BATCH
             loss = 0
-            item_embeddings.detach_()
+            item_embeddings.detach_()  # Detachment is needed to prevent double propagation of gradient
             user_embeddings.detach_()
             item_embeddings_timeseries.detach_()
             user_embeddings_timeseries.detach_()
 
+            # REINITIALIZE
+            reinitialize_tbatches()
+            tbatch_to_insert = -1
 
 json.dump(validation_ranks, open('results/validation_ranks_%s_%s_%s.json' % (args.epoch, args.model, args.network), 'w'))
 json.dump(test_ranks, open('results/test_ranks_%s_%s_%s.json' % (args.epoch, args.model, args.network), 'w'))
